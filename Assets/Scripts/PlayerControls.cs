2942775420b3dc98db1c8146fd176bf84b8c4a65
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2024-04-04 17:42:08 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    //Game manager object
+    [Header("Game Controller Object for controlling the game")]
+    public GameController gameController;
+    [Header("Default Velocity")]
+    public float velocity = 5;
+    //Physics for the bird
+    private Rigidbody2D rb;
+    //height of the bird object on the y axis
+    private float objectHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Game Controller conponent
+        gameController = GetComponent<GameController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
